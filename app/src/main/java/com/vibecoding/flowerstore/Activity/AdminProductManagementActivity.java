131b0f5abf5478f14682eea6cc9736b5d45d75dd
gbao261205 <ngbao261205@gmail.com>
2025-12-29 20:14:31 +0700

    Admin Product Management layout

---
+++ app/src/main/java/com/vibecoding/flowerstore/Activity/AdminProductManagementActivity.java
@@ -0,0 +1,24 @@
+package com.vibecoding.flowerstore.Activity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.vibecoding.flowerstore.R;
+
+public class AdminProductManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title not the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//int flag, int mask
+        setContentView(R.layout.activity_admin_product_management);
+    }
+}
